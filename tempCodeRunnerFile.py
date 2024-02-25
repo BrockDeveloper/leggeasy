aa373a5cff3f76bc1bb0fbab44b0301014810a3a
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2024-02-25 01:19:57 +0100

    Test: cpp parser

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+                # if content:
+                #     ps = content.findall(PREFIX + "p")
+                    
+                #     for p in ps:
+                #         for t in p.itertext():
+                #             print(t)
