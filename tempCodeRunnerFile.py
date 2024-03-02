59ac608602b7572e55befdc585800b0372f15275
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2024-03-02 21:20:12 +0100

    Fix: git ignore

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-                # if content:
-                #     ps = content.findall(PREFIX + "p")
-                    
-                #     for p in ps:
-                #         for t in p.itertext():
-                #             print(t)
