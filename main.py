7c03d13cf3161b5ee19df6f324d74322893f2901
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2024-03-08 00:01:37 +0100

    Fix: api configuration

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ def get_list(document: str):
 
 if __name__ == "__main__":
     
-    uvicorn.run("main:app", host="localhost", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
